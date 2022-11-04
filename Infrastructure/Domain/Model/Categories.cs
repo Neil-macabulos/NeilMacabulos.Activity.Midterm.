e54f171592b76e7c4713648909436f5b7881ed76
NEIL R. MACABULOS <NEIL2003>
2022-11-04 16:24:40 +0800

    activity1 midterm

---
+++ Infrastructure/Domain/Model/Categories.cs
@@ -0,0 +1,10 @@
+﻿namespace NeilMacabulos.Midterm_.Infrastructure.Domain.Model
+{
+    public class Categories
+    {
+        public Guid? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Abbreviation { get; set; }
+    }
+}
